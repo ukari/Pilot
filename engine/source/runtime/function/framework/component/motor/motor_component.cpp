58dc345deafcb22eb17c446544194abf4b272442
chendianbuji <chendianbuji@gmail.com>
2022-07-26 21:06:36 +0800

    update

--- engine/source/runtime/function/framework/component/motor/motor_component.cpp
+++ engine/source/runtime/function/framework/component/motor/motor_component.cpp
@@ -132,9 +132,11 @@ namespace Piccolo
             if ((unsigned int)GameCommand::jump & command)
             {
                 m_jump_state                  = JumpState::rising;
-                m_vertical_move_speed         = Math::sqrt(m_motor_res.m_jump_height * 2 * gravity);
+                //m_vertical_move_speed         = Math::sqrt(m_motor_res.m_jump_height * 2 * gravity);
+                m_vertical_move_speed = m_motor_res.m_vertical_move_speed;
                 std::cout << "m_motor_res.m_vertical_move_speed: " << m_motor_res.m_vertical_move_speed << std::endl;
                 std::cout << "m_vertical_move_speed: " << m_vertical_move_speed << std::endl;
+                
                 m_jump_horizontal_speed_ratio = m_move_speed_ratio;
             }
             else
